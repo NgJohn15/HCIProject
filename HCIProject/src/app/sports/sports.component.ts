c88142d27dc3ada51d29ebccad4646944fa15ea8
NgJohn15 <ngjohn15@gmail.com>
2022-03-17 17:46:01 -0400

    temp update

--- HCIProject/src/app/sports/sports.component.ts
+++ HCIProject/src/app/sports/sports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalConstants } from 'src/global-constants';
 
 @Component({
   selector: 'app-sports',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sports.component.css']
 })
 export class SportsComponent implements OnInit {
+
+  items = GlobalConstants.items;
 
   constructor() { }
 
