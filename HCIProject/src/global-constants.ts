c8831fab96b8e3636aec650963c57f3d951fa9d7
NgJohn15 <ngjohn15@gmail.com>
2022-03-17 16:58:02 -0400

    fix: changed item layout to grid

--- HCIProject/src/global-constants.ts
+++ HCIProject/src/global-constants.ts
@@ -1,8 +1,19 @@
 export class GlobalConstants {
     public static count: number = 0;
     public static items = new Map([
-        [ "soccer_ball", {name: "soccer ball", dept: "sports"}],
-        [ "personal_computer", {name: "personal computer", dept: "electronics"}],
-        [ "jacket", {name: "jacket", dept: "clothes"}]
+        [ "soccer_ball", {name: "Soccer Ball", dept: "sports", description: "Standard regulation sized soccer ball for adults."}],
+        [ "foot_ball", {name: "Football", dept: "sports", description: "Standard leather regulation sized football for adults."}],
+        [ "volleyball", {name: "Volleyball", dept: "sports", description: "Standard issue volleyball for all ages."}],
+        [ "basketball", {name: "Baskestball", dept: "sports", description: "Standard regulation mens basketball."}],
+        [ "baseball", {name: "Baseball", dept: "sports", description: "Standard regulation baseball."}],
+        [ "football_helmet", {name: "Football Helment", dept: "sports", description: "Football helment for men. Sizes include S, M, and L."}],
+        [ "shin_guards", {name: "Soccer Shinguards", dept: "sports", description: "Soccer shinguiards to protect your shins. One size fits all!"}],
+        [ "personal_computer", {name: "personal computer", dept: "electronics", description: "Computer capable of your everyday needs."}],
+        [ "monitor", {name: "SUSA 27 inch gaming monitor", dept: "electronics", description: "High refresh rate gaming monitor with High Defintion display."}],
+        [ "smartdevice", {name: "Goggle Alicia", dept: "electronics", description: "Upgrade your home with this smart device! Uses artificial intelligence to answer any question."}],
+        [ "computer_mouse", {name: "Computer Mouse", dept: "electronics", description: "USB computer mouse."}],
+        [ "computer_keyboard", {name: "Computer Keyboard", dept: "electronics", description: "USB standard keyboard. QWERTY layout."}],
+        [ "graphics_card", {name: "MVIDA RTX 3090 Graphics Card", dept: "electronics", description: "Elusive and extremely rare super powerful graphics card. Supports ray tracing and high computing."}],
+        [ "jacket", {name: "jacket", dept: "clothes", description: "Warm comfy uni-sex jacket."}]
     ]);
 }
